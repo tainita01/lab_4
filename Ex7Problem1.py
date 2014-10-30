be81ef45a6214e117eed420d2e31cb1b2063d995
tainita01 <taina@uw.edu>
2014-10-29 22:18:14 -0700

    Exercise7Challenge1

---
+++ Ex7Problem1.py
@@ -0,0 +1,12 @@
+import arcpy
+from arcpy import env
+env.workspace = "F:/UWTacoma/GIS_501_AU_2014/lab_4/data/Exercise07/"
+fc = "airports.shp"
+delimitedField = arcpy.AddFieldDelimiters(fc, "FEATURE")
+cursor = str(arcpy.da.SearchCursor(fc,"FEATURE", delimitedField +" = 'Airport'"))
+for row in cursor:
+    unique_name = arcpy.CreateUniqueName("Results/airport_buffer.shp")
+    arcpy.Buffer_analysis(fc, unique_name, "15000 METERS")    
+else:
+    unique_name = arcpy.CreateUniqueName("Results/seaplane_buffer.shp")
+    arcpy.Buffer_analysis(fc, unique_name, "7500 METERS")
